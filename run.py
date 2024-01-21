9742287368ad5e489dff12dd14afd8dbc8d0ef27
Husnain Raza <hr_11@hotmail.co.uk>
2024-01-21 21:44:02 +0000

    get last five

--- run.py
+++ run.py
@@ -76,6 +76,19 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+def get_5():
+    """
+    get the last five days of data
+    """
+    sales =  SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+    
+
 def main():
     """
     program functions
@@ -87,5 +100,7 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
     
 
-print("\nWelcome to love sandwiches data automation service.\n")
-main()
+#print("\nWelcome to love sandwiches data automation service.\n")
+#main()
+sales_columns = get_5()
+
