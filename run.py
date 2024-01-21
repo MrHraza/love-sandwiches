2c51457864861f816a6f07477938a2fa6e8e08db
Husnain Raza <hr_11@hotmail.co.uk>
2024-01-21 18:12:52 +0000

    appended new row of data to the sales worksheet

--- run.py
+++ run.py
@@ -25,9 +25,11 @@ def get_sales_data():
         data_str = input("Enter you data here: ")
         
         sales_data = data_str.split(",")
+
         if validate_data(sales_data):
             print("Data is valid")
             break
+
     return sales_data
 
 def validate_data(values):
@@ -43,8 +45,20 @@ def validate_data(values):
                 f"Exactly six values are required, you have provided {len(values)}")
     except ValueError as e:
         print(f"invalid data: {e}, please try again.\n")
+        return False
+    
+    return True
 
-
-
+def update_worksheet_data(data):
+    """
+    updating sales worksheet
+    """
+    print("updating worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet has been updated successfully.\n")
 
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+
+update_worksheet_data(sales_data)
